e4255800ec6f6405fa6bb3642e72f05b07690856
nk17kumar <nk17kumar@gmail.com>
2018-03-15 02:00:34 +0530

    added demo package

---
+++ src/main/java/com/app/demo/Driver.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.app.demo;
+
+/**
+ * @author nk17kumar
+ *
+ */
+public class Driver {
+
+	/**
+	 * 
+	 */
+	public Driver() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
