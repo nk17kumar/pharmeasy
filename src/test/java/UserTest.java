c992d1e8941c5ced04abae77ae8c427831524318
nk17kumar <nk17kumar@gmail.com>
2018-03-16 01:06:29 +0530

    added user class JUnits

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,5 +1,11 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.management.InvalidAttributeValueException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -14,7 +20,7 @@ public class UserTest {
 	private static Doctor d;
 	private static Pharmacist ph;
 	private static Logger log;
-	
+	private static Prescription pr;
 	
 	public void createDataMembers() {
 		
@@ -31,6 +37,68 @@ public class UserTest {
 				
 		PersonalDetails info3 = new PersonalDetails("Mohan",22,"male","B+","xyz@abc.com","1234","abc area");
 	    ph= new Pharmacist(info3,"ayurvedic medicines");
+	    
+	    Date date1 = new Date(System.currentTimeMillis());
+		List < String > medi1 = new ArrayList <String>();
+		medi1.add("Combiflame");
+		medi1.add("amlopinAT");
+		String inst1 = "1 tab morning and evening";
+		String extraInst1 = "bed rest";
+		pr = new Prescription(date1,medi1,inst1,extraInst1);
+	   
 	}
+	
+	@Test(expected = NullPointerException.class)
+	public void getPreviousPrescriptionTestNoPrescriptionFailure() throws InvalidAttributeValueException, UnsupportedOperationException {
+		createDataMembers();
+		p.approveRequests(ph);
+		ph.getPreviousPrescription(p);
+		
+	}
+	
+	@Test(expected = InvalidAttributeValueException.class)
+	public void getPreviousPrescriptionTestAccessFailure() throws InvalidAttributeValueException, UnsupportedOperationException {
+		createDataMembers();
+		ph.getPreviousPrescription(p);
+	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void getPreviousPrescriptionTestUnsupportedAccessFailure() throws InvalidAttributeValueException, UnsupportedOperationException {
+		createDataMembers();
+		Patient p1 = p;
+		p.getPreviousPrescription(p1);
+	}
+	
+	@Test
+	public void getPreviousPrescriptionDoctorSuccess() throws InvalidAttributeValueException, UnsupportedOperationException {
+		createDataMembers();
+		d.givePrescription(p, pr);
+		p.approveRequests(ph);
+		Prescription tmp = ph.getPreviousPrescription(p);
+		assertEquals(tmp,pr);
+	}
+	
+	@Test
+	public void getPreviousPrescriptionPharmacistSuccess() throws InvalidAttributeValueException, UnsupportedOperationException {
+		createDataMembers();
+		d.givePrescription(p, pr);
+		p.approveRequests(d);
+		Prescription tmp = d.getPreviousPrescription(p);
+		assertEquals(tmp,pr);
+	}
+	
+	@Test
+	public void getPreviousPrescriptionMatch() throws InvalidAttributeValueException, UnsupportedOperationException {
+		createDataMembers();
+		d.givePrescription(p, pr);
+		p.approveRequests(ph);
+		p.approveRequests(d);
+		Prescription tmp1 = ph.getPreviousPrescription(p);
+		Prescription tmp2 = ph.getPreviousPrescription(p);
+		assertEquals(tmp1,tmp2);
+	}
+	
+	
+	
 	
 }
